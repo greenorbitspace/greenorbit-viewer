6f899e766ce8060228d411a3b3b1b7833908388e
rjmlaird <rjmlaird@gmail.com>
2025-07-09 03:18:01 +0100

    fix lint

--- gulpfile.js
+++ gulpfile.js
@@ -193,6 +193,7 @@ gulp.task("lint", function (done) {
   runExternalModule(eslintExecutable, [
     "--max-warnings",
     "0",
+    "--fix"
     "terriajs/buildprocess/**/*.js",
     "terriajs/lib/**/*.js"
   ]);
